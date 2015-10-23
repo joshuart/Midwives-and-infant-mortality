09338dae298248b82466d2ce3507ed80f4075c8f
Joshua Taylor <joshuataylor@Joshuas-MBP.netgear.com>
2015-10-23 16:33:38 -0400

    looking more closely at the data

--- Code/data01.R
+++ Code/data01.R
@@ -4,3 +4,13 @@
 # Author: Josh Taylor
 # Last edited: 10/21/15
 ######################################################################
+library(data.table)
+
+
+DT = read.csv("/Volumes/Seagate Data Drive/Research/Midwives:Doctors Outcomes/Infant Mortality/Data/linkco1995us_den.csv", 
+                header = T, nrows = 1000)
+
+
+
+
+
---
+++ Code/stat01.R
@@ -0,0 +1,32 @@
+######################################################################
+# Program: stat01.R
+# Project: Midwives/Infant Mortality
+# Author: Josh Taylor
+# Last edited: 10/21/15
+######################################################################
+
+###### Details #######################################################
+# Regression I want to run:
+#   1. mort ~ covariates + midwife_dummy
+#   2. apgar ~ covariates + midwife_dummy
+#   3. apgar ~ covariates + midwife_dummy (quantile)
+#   4. mort ~ covaritaes + midwife_dummy (preterm)
+#   
+# Data I need to perform the analysis:
+#   1. birth_outcome
+#   2. birth_attendent
+#   3. state
+#   4. year
+#   5. race_baby/race_mother/race_father
+#   6. sex
+#   7. gestation (?)
+#   8. birthweight (?)
+#   9. plurality
+#   10. age_mother/age_mother_recode
+#   11. education_mother
+#   12. interval_since_last_live_birth
+#   13. outcome_last_pregnancy
+#   14. health_risks
+#   15. c_section
+#   16. delivery method
+######################################################################
