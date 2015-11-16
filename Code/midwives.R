29c5b9687bae875401f26149ac569e91514c4aa8
joshuart <joshuataylor@wl-10-190-103-71.wireless.duke.edu>
2015-11-16 13:12:39 -0500

    small changes

--- Code/midwives.R
+++ Code/midwives.R
@@ -2,7 +2,7 @@
 # Program: data01.R
 # Project: Midwives/Infant Mortality
 # Author: Josh Taylor
-# Last edited: 11/5/15
+# Last edited: 11/16/15
 ######################################################################
 
 source('C:\Users\jt190\Box Sync\Home Folder jt190\Research\Midwives\Code\data01.R')
